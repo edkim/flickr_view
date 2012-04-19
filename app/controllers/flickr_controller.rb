83d0d7097f42f3b7f90eb98f045797af8a1f4e11
Edward Kim <edk217@gmail.com>
2012-04-19 15:28:28 -0400

    move Flickraw initializers to config file

--- app/controllers/flickr_controller.rb
+++ app/controllers/flickr_controller.rb
@@ -9,16 +9,13 @@ class FlickrController < ApplicationController
   def show
   	redirect_to "/" if !user_signed_in?
   	
-  	FlickRaw.api_key="5448acb0be52ab7d71af7d0498829ea6"
-	FlickRaw.shared_secret="9552411e9c7dd486"
+		a = 0
+		@urls = Array.new
+		@flickr_links = Array.new
 
-	a = 0
-	@urls = Array.new
-	@flickr_links = Array.new
+		list = flickr.interestingness.getList per_page: 10
 
-	list = flickr.interestingness.getList per_page: 10
-
-	list.each do |photo|	  
+		list.each do |photo|	  
 	  @urls[a] = FlickRaw.url_m(flickr.photos.getInfo(:photo_id => "#{photo.id}"))
 	  @flickr_links[a] = FlickRaw.url_photopage(flickr.photos.getInfo(:photo_id => "#{photo.id}"))
 	  a+=1	  
